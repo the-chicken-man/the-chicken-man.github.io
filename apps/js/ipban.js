e722e1ed291f3fb77cdfa34f61cad892bc065667
The chicken  MAN <856030@dpsk12.net>
2025-05-08 19:45:46 -0600

    coolstuff

--- apps/js/ipban.js
+++ apps/js/ipban.js
@@ -1,24 +1,16 @@
-const bannedIPs = [
-            '73.14.78.150',
-            //i will add more if you mean
-        ];
 
-        async function checkIP() {
-            try {
-                const response = await fetch('https://api64.ipify.org?format=json');
-                const data = await response.json();
-                const userIP = data.ip;
+  const bannedIPs = ["73.14.78.150"];
 
-                if (bannedIPs.includes(userIP)) {
-                    document.body.innerHTML = <embed src="./ipban.html" height="100%" width="100%"></embed>;
-                }
-            } catch (error) {
-                console.error('Failed to get IP:', error);
-            }
-        }
+  fetch("https://api.ipify.org?format=json")
+    .then(res => res.json())
+    .then(data => {
+      const userIP = data.ip;
 
-        function showAppealForm() {
-            document.getElementById('appealForm').style.display = 'block';
-        }
+      if (bannedIPs.includes(userIP)) {
+        
+        window.location.href = "ipban.html";
 
-        window.onload = checkIP;
+      
+      }
+    });
+
